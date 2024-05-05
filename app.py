1ca5c453d7d8c88be3071fc00866d7995a345d29
Charishma Tata <153920644+charishma101@users.noreply.github.com>
2024-05-05 19:56:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -112,15 +112,8 @@ def main():
 
     class VideoProcessor:
 	def recv(self, frame):
-		#frm = frame.to_ndarray(format="bgr24")
+		
         tflite_detect_images(frame, PATH_TO_MODEL, PATH_TO_LABELS)
-
-		#faces = cascade.detectMultiScale(cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY), 1.1, 3)
-
-		#for x,y,w,h in faces:
-			#cv2.rectangle(frm, (x,y), (x+w, y+h), (0,255,0), 3)
-
-		#return av.VideoFrame.from_ndarray(frm, format='bgr24')
 
     webrtc_streamer(key="key", video_processor_factory=VideoProcessor,rtc_configuration=RTCConfiguration({"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}))
     
